20daf78054871f7a01ca28c7649c24bc77ac1963
Heiler Nova <heilernov@outlook.com>
2024-05-10 11:39:30 -0500

    Servicio para el manejo de las peticiones con la base de datos

---
+++ backend/api/src/common/common.module.ts
@@ -0,0 +1,13 @@
+import { Global, Module } from '@nestjs/common';
+import { ConnectionDbService } from './connection-db';
+
+const services = [
+    ConnectionDbService
+];
+
+@Global()
+@Module({
+    providers: services,
+    exports: services
+})
+export class CommonModule {}
