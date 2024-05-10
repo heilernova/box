cd8379faeaf75ef64a4d1c502b39a993680a6579
Heiler Nova <heilernov@outlook.com>
2024-05-10 11:13:43 -0500

    Implementamos la configuraciones

--- backend/api/src/main.ts
+++ backend/api/src/main.ts
@@ -1,8 +1,16 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { HttpAdapterHost, NestFactory } from '@nestjs/core';
+import { AppModule } from '@app/app.module';
+import { appInitConfig } from '@app/config';
+import { AllExceptionsFilter, AppResponseInterceptor } from '@app/config/interceptors';
 
 async function bootstrap() {
+  appInitConfig();
+  const port: string = process.env.PORT ?? '3000';
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const { httpAdapter } = app.get(HttpAdapterHost);
+  app.useGlobalFilters(new AllExceptionsFilter(httpAdapter));
+  app.useGlobalInterceptors(new AppResponseInterceptor());
+  await app.listen(port);
+  console.log(`[APP] running in port ${port}`);
 }
 bootstrap();
