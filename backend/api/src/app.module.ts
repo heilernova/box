cb37447598c0a3895efcb019c7360d13a67dff02
Heiler Nova <heilernov@outlook.com>
2024-05-10 13:42:23 -0500

    Se creamo el módulo para almacenar los controladores

--- backend/api/src/app.module.ts
+++ backend/api/src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CommonModule } from './common/common.module';
+import { ControllersModule } from './controllers/controllers.module';
 
 @Module({
-  imports: [CommonModule],
+  imports: [CommonModule, ControllersModule],
   controllers: [AppController],
   providers: [AppService],
 })
