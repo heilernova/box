540578a62b5cc68a23900eaa1e4b532b88ee6a40
Heiler Nova <heilernova@outlook.com>
2024-06-25 16:35:56 -0500

    Add(Website): actualiación automatica

--- website/src/app/app.component.ts
+++ website/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { SessionService } from './common/session';
+import { SwUpdate } from '@angular/service-worker';
 
 @Component({
   selector: 'app-root',
@@ -10,10 +11,17 @@ import { SessionService } from './common/session';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'website';
+  private readonly _sw = inject(SwUpdate);
   private readonly _session = inject(SessionService);
 
   constructor(){
     this._session.init();
+    this._sw.checkForUpdate().then(res => {
+      if (res){
+        this._sw.activateUpdate().then(() => {
+          location.reload();
+        })
+      }
+    })
   }
 }
