bad6cc07aa1dd230552bbcab3ff94428a06666a1
Heiler Nova <heilernova@outlook.com>
2024-06-25 13:52:21 -0500

    Add(Website): proveedor HttpClient

--- website/src/app/app.config.ts
+++ website/src/app/app.config.ts
@@ -5,10 +5,19 @@ import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideServiceWorker } from '@angular/service-worker';
+import { provideHttpClient, withInterceptors, withFetch } from '@angular/common/http';
+import { apiInterceptor } from './common/api/api.interceptor';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(), provideAnimationsAsync(), provideServiceWorker('ngsw-worker.js', {
-            enabled: !isDevMode(),
-            registrationStrategy: 'registerWhenStable:30000'
-          })]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideClientHydration(),
+    provideAnimationsAsync(),
+    provideHttpClient(withFetch(), withInterceptors([apiInterceptor])),
+    provideServiceWorker('ngsw-worker.js', {
+      enabled: !isDevMode(),
+      registrationStrategy: 'registerWhenStable:30000'
+    })
+  ],
 };
