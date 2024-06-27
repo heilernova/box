fff7b1c7c3a8d63bf7655a0dba92fb10646fa048
Heiler Nova <heilernova@outlook.com>
2024-06-27 15:23:46 -0500

    Fix(API): no se estaban filtrando los ejercicios de RM

--- api/src/common/models/users/user-record-rms/user-record-rms.service.ts
+++ api/src/common/models/users/user-record-rms/user-record-rms.service.ts
@@ -7,7 +7,7 @@ export class UserRecordRmsService {
     constructor(private readonly _db: ConnectionDbService){}
 
     async get(id: UUID): Promise<IUserRecordRM[]> {
-        let sql: string = 'select a.id as workout_id, a.name_in_english, a.name_in_spanish, a.slug, a.abbreviation, b.record_id, b.create_at,	b.weight_in_kilos,	b.weight_in_pounds from workouts a left join (select * from vi_users_rms where user_id = $1) b on b.workout_id = a.id order by a.name_in_english' //'select record_id, workout_id, create_at, name_in_english, name_in_spanish, slug, abbreviation, weight_in_kilos, weight_in_pounds from vi_users_rms where user_id = $1';
+        let sql: string = 'select a.id as workout_id, a.name_in_english, a.name_in_spanish, a.slug, a.abbreviation, b.record_id, b.create_at,	b.weight_in_kilos,	b.weight_in_pounds from workouts a left join (select * from vi_users_rms where user_id = $1) b on b.workout_id = a.id where a.rm = true order by a.name_in_english' //'select record_id, workout_id, create_at, name_in_english, name_in_spanish, slug, abbreviation, weight_in_kilos, weight_in_pounds from vi_users_rms where user_id = $1';
         return (await this._db.query(sql, [id])).rows;
     }
 
