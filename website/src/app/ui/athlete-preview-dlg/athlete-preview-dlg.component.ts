c192f3d95f76de9d630575ca866b20d09cfe76df
Heiler Nova <heilernova@outlook.com>
2024-06-27 14:55:39 -0500

    Add(Website): animación de carga de RMs

--- website/src/app/ui/athlete-preview-dlg/athlete-preview-dlg.component.ts
+++ website/src/app/ui/athlete-preview-dlg/athlete-preview-dlg.component.ts
@@ -1,24 +1,31 @@
 import { Component, Inject, signal } from '@angular/core';
-import { MAT_DIALOG_DATA, MatDialogModule } from '@angular/material/dialog';
 import { Athlete } from '@app/common/data/athletes';
 import { AgePipe } from '@app/common/pipes';
 import { IRm } from '@app/common/session/User.model';
+
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MAT_DIALOG_DATA, MatDialogModule } from '@angular/material/dialog';
+
 
 @Component({
   selector: 'app-athlete-preview-dlg',
   standalone: true,
   imports: [
     MatDialogModule,
-    AgePipe
+    AgePipe,
+    MatProgressSpinnerModule
   ],
   templateUrl: './athlete-preview-dlg.component.html',
   styleUrl: './athlete-preview-dlg.component.scss'
 })
 export class AthletePreviewDlgComponent {
   public readonly rms = signal<IRm[]>([]);
+  public readonly loadingRMs = signal<boolean>(true);
   constructor(@Inject(MAT_DIALOG_DATA) public readonly data: Athlete){
-    this.data.getRMs().then(list => {
-      this.rms.set(list);
+
+    this.data.getRMs().then(list => {      
+      this.rms.set(list.filter(x => x.record));
+      this.loadingRMs.set(false);
     })
   }
 }
