7792dcc2c39824f92b75897c663a3b61746a7670
Heiler Nova <heilernova@outlook.com>
2024-06-28 17:38:04 -0500

    Add(Website): country

--- website/src/app/ui/message/message.service.ts
+++ website/src/app/ui/message/message.service.ts
@@ -9,19 +9,19 @@ export class MessageService {
   private _snackBar: MatSnackBar = inject(MatSnackBar);
   constructor() { }
 
-  success(message: string){
-    return this._snackBar.open(message, undefined, {  duration: 2000, verticalPosition: 'top' });
+  success(message: string, options?: { duration?: number }){
+    return this._snackBar.open(message, undefined, {  duration: options?.duration ?? 2000, verticalPosition: 'top' });
   }
 
-  show(message: string){
-    return this._snackBar.open(message, undefined, {  duration: 2000, verticalPosition: 'top' });
+  show(message: string, options?: { duration?: number }){
+    return this._snackBar.open(message, undefined, {  duration: options?.duration ?? 2000, verticalPosition: 'top' });
   }
 
-  error(message: string){
-    return this._snackBar.open(message, undefined, {  duration: 2000, verticalPosition: 'top' });
+  error(message: string, options?: { duration?: number }){
+    return this._snackBar.open(message, undefined, {  duration: options?.duration ?? 2000, verticalPosition: 'top' });
   }
 
-  warning(message: string){
-    return this._snackBar.open(message, undefined, {  duration: 2000, verticalPosition: 'top' });
+  warning(message: string, options?: { duration?: number }){
+    return this._snackBar.open(message, undefined, {  duration: options?.duration ?? 2000, verticalPosition: 'top' });
   }
 }
--- website/src/app/ui/sign-up/sign-up.component.ts
+++ website/src/app/ui/sign-up/sign-up.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { Router, RouterLink } from '@angular/router';
 
@@ -6,9 +6,16 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatInputModule } from '@angular/material/input';
 import { MatSelectModule } from '@angular/material/select';
 import { MatDividerModule } from '@angular/material/divider';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+
+import { SessionService } from '@app/common/session';
+import { DataCountriesService } from '@app/common/data/locations';
+
 import { MatInputNumber } from '../mat-input-number';
 import { MatInputCellphone } from '../mat-input-cellphone';
-import { SessionService } from '@app/common/session';
+import { MessageService } from '../message';
+
+
 @Component({
   selector: 'app-sign-up',
   standalone: true,
@@ -20,14 +27,19 @@ import { SessionService } from '@app/common/session';
     MatSelectModule,
     MatDividerModule,
     MatInputNumber,
-    MatInputCellphone
+    MatInputCellphone,
+    MatCheckboxModule
   ],
   templateUrl: './sign-up.component.html',
   styleUrl: './sign-up.component.scss'
 })
 export class SignUpComponent {
+  private readonly _dataCountries = inject(DataCountriesService);
   private readonly _session = inject(SessionService);
   private readonly _router = inject(Router);
+  private readonly _message = inject(MessageService);
+
+  public readonly countries = signal<any[]>([]);
 
   public readonly formGroup = new FormGroup({
     name: new FormControl<string>('', { nonNullable: true, validators: Validators.required }),
@@ -37,6 +49,7 @@ export class SignUpComponent {
     sex: new FormControl<'M' | 'F' | null>(null, { validators: Validators.required }),
     tall: new FormControl<number | null>(null, { validators: Validators.required }),
     weight: new FormControl<number | null>(null, { validators: Validators.required }),
+    country: new FormControl<string | null>(null, { validators: Validators.required }),
     email: new FormControl<string | null>(null, { validators: Validators.required }),
     cellphone: new FormControl<string | null>(null, { validators: Validators.required }),
     username: new FormControl<string | null>(null, { validators: Validators.required }),
@@ -45,7 +58,9 @@ export class SignUpComponent {
   });
 
   constructor(){
-
+    this._dataCountries.getAll().then(list => {
+      this.countries.set(list);
+    })
   }
 
   onSave(): void {
@@ -55,6 +70,11 @@ export class SignUpComponent {
     }
 
     let values = this.formGroup.getRawValue();
+
+    if (values.password != values.confirmPassword){
+      this._message.error('Las contraseñas con coincide');
+      return;
+    }
 
     this._session.signUp({
       name: values.name,
