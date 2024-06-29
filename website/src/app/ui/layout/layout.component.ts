7bbcb19bebb688b2bf2be0396729f88a0b98e2d9
Heiler Nova <heilernova@outlook.com>
2024-06-29 16:00:20 -0500

    Update(Website): layout

--- website/src/app/ui/layout/layout.component.ts
+++ website/src/app/ui/layout/layout.component.ts
@@ -5,6 +5,15 @@ import { SessionService } from '@app/common/session';
 import { User } from '@app/common/session/User.model';
 
 import { MatMenuModule } from '@angular/material/menu';
+import { MatRippleModule } from '@angular/material/core';
+
+const NAV_FOR_WEB: { name: string, icon: string, link: string }[] = [
+  { name: 'Ejercicios', icon: 'fa-solid fa-dumbbell', link: '/ejercicios' },
+  { name: 'WODs', icon: 'fa-solid fa-weight-hanging', link: '/wods' },
+  { name: "Gym's", icon: 'fa-solid fa-box', link: '/gyms' },
+  { name: "Atletas", icon: 'fa-solid fa-people-group', link: '/atletas' },
+  { name: "Competencias", icon: 'fa-solid fa-medal', link: '/competencias' },
+];
 
 @Component({
   selector: 'app-layout',
@@ -14,7 +23,8 @@ import { MatMenuModule } from '@angular/material/menu';
     RouterLinkActive,
     RouterOutlet,
     MatButtonModule,
-    MatMenuModule
+    MatMenuModule,
+    MatRippleModule
   ],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.scss'
@@ -23,14 +33,18 @@ export class LayoutComponent {
   private readonly _session = inject(SessionService);
   public readonly user = signal<User| null>(null);
   public readonly navItems = signal<{ name: string, link: string }[]>([]);
+  public readonly navForWeb = signal<{ name: string, icon: string, link: string }[]>(NAV_FOR_WEB);
 
   constructor(){
     this._session.changeUser.subscribe(value => {
       this.user.set(value);
       if (value){
-        this.navItems.set([{ name:'rms', link: 'rms'  }]);
+        this.navForWeb.update(list => [{ name: 'RMs', link: 'rms', icon: 'fa-solid fa-clipboard-list' } ,...list])
       } else {
-        this.navItems.set([]);
+        this.navForWeb.update(list => {
+          list.splice(0, 1);
+          return list;
+        })
       }
     })
   }
